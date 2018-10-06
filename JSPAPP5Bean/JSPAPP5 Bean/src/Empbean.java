445c92181d7bb0059a320382295988bfdaedc1b6
Lakshmanan <42428608+hereinlaksh@users.noreply.github.com>
2018-10-06 21:54:13 +0530

    Add files via upload
    First

---
+++ JSPAPP5Bean/JSPAPP5 Bean/src/Empbean.java
@@ -0,0 +1,27 @@
+import java.io.Serializable;
+
+public class Empbean implements Serializable{
+	
+	private int eid;
+	public int getEid() {
+		return eid;
+	}
+	public void setEid(int eid) {
+		this.eid = eid;
+	}
+	public String getEname() {
+		return ename;
+	}
+	public void setEname(String ename) {
+		this.ename = ename;
+	}
+	public Double getEsal() {
+		return esal;
+	}
+	public void setEsal(Double esal) {
+		this.esal = esal;
+	}
+	private String ename;
+	private Double esal;
+
+}
